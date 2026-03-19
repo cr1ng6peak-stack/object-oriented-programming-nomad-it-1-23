553c866b0137a9ba8a5f2d054133cbf060ac5e3c
cr1ng6peak-stack <cr1ng6peak@gmail.com>
2026-03-19 22:18:26 +0600

    Initial commit

---
+++ fields.java
@@ -0,0 +1,29 @@
+public class fields {
+
+    private int size;
+
+    public fields_1(int size) {
+        this.size = size;
+    }
+    public void printField(Turtle turtle) {
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+
+                if (i == turtle.getX() && j == turtle.getY()) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(". ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+
+  
+    public int getSize() {
+        return size;
+    }
+}
